e4cc36e1c64283a2f1a0c3e506647f1ed1451996
Ayan-777 <aymondal77@gmail.com>
2024-11-16 23:04:52 +0530

    change for mongoDB bug solev

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 
 
 dotenv.config({
     path: './env'
-})
+});
 
 
-connectDB()
+connectDB();
 
 /*
 import express from "express";
