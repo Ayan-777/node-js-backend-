5ad22e04c9ae620c9c4e55b355d43a301fb90933
Ayan-777 <mondalayan091@gmail.com>
2025-01-11 20:50:25 +0530

    fix: correct jwt import and clean up index.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -189,7 +189,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     }
 
     try {
-     const decodedToken = Jwt.verify(
+     const decodedToken = jwt.verify(
          incomingRefreshToken,
          process.env.REFRESH_TOKEN_SECRET
      )
--- src/index.js
+++ src/index.js
@@ -21,30 +21,3 @@ connectDB()
   .catch((err) => {
     console.error('MONGODB connection failed !!', err);
   });
-
-
-
-
-  
-
-
-  
-// If you want to keep the commented-out code, make sure it's properly commented out:
-/*
-(async () => {
-  try {
-    await mongoose.connect(`process.env.MONGODB_URI/${DB_NAME}`);
-    app.on('error', (err) => {
-      console.log('ERROR:', err);
-      throw err;
-    });
-
-    app.listen(process.env.PORT, () => {
-      console.log(`Server is running on port ${process.env.PORT}`);
-    });
-  } catch (error) {
-    console.error('ERROR:', error);
-    throw error;
-  }
-})();
-*/
